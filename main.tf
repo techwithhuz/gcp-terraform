a039f00ff19b05e4ef01ba012a0376122931e489
techwithhuz <138496939+techwithhuz@users.noreply.github.com>
2025-06-08 22:01:05 +0530

    Create main.tf with VM instance, VPC, subnet, firewall, and enable APIs

--- main.tf
+++ main.tf
@@ -11,75 +11,6 @@ terraform {
 provider "google" {
   project = var.project_id
   region  = var.region
-  zone    = var.zone
-}
-
-resource "google_project_service" "compute_api" {
-  service = "compute.googleapis.com"
-  disable_on_destroy = false
-  depends_on = []
-  labels = {
-    creator = "gcp-terraform-agent"
-  }
-}
-
-resource "google_compute_network" "custom_network" {
-  name                    = "custom-network"
-  auto_create_subnetworks = false
-  labels = {
-    creator = "gcp-terraform-agent"
-  }
-}
-
-resource "google_compute_subnetwork" "custom_subnetwork" {
-  name          = "custom-subnetwork"
-  ip_cidr_range = "10.0.0.0/24"
-  network       = google_compute_network.custom_network.id
-  region        = var.region
-  labels = {
-    creator = "gcp-terraform-agent"
-  }
-}
-
-resource "google_compute_firewall" "ssh_firewall" {
-  name    = "allow-ssh"
-  network = google_compute_network.custom_network.name
-  allows {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-  source_ranges = ["0.0.0.0/0"]
-  direction    = "INGRESS"
-  labels = {
-    creator = "gcp-terraform-agent"
-  }
-}
-
-resource "google_compute_instance" "default" {
-  name         = "vm-instance"
-  machine_type = "e2-micro"
-  zone         = var.zone
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    network    = google_compute_network.custom_network.name
-    subnetwork = google_compute_subnetwork.custom_subnetwork.name
-
-    access_config {
-      // Ephemeral public IP
-    }
-  }
-
-  labels = {
-    creator = "gcp-terraform-agent"
-  }
-
-  depends_on = [google_project_service.compute_api]
 }
 
 variable "project_id" {
@@ -93,13 +24,70 @@ variable "region" {
   default     = "us-central1"
 }
 
-variable "zone" {
-  description = "The GCP zone"
-  type        = string
-  default     = "us-central1-a"
+resource "google_project_service" "compute_api" {
+  project = var.project_id
+  service = "compute.googleapis.com"
+  depends_on = []
+  disable_on_destroy = false
 }
 
-output "instance_ip" {
-  description = "The external IP of the VM instance"
-  value       = google_compute_instance.default.network_interface[0].access_config[0].nat_ip
+resource "google_compute_network" "custom_network" {
+  name                    = "custom-vpc-network"
+  auto_create_subnetworks = false
+  project                 = var.project_id
+  labels = {
+    creator = "gcp-terraform-agent"
+  }
+}
+
+resource "google_compute_subnetwork" "custom_subnet" {
+  name          = "custom-subnet"
+  ip_cidr_range = "10.10.0.0/24"
+  region        = var.region
+  network       = google_compute_network.custom_network.id
+  project       = var.project_id
+
+  labels = {
+    creator = "gcp-terraform-agent"
+  }
+}
+
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.custom_network.id
+  project = var.project_id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+
+  direction = "INGRESS"
+  labels = {
+    creator = "gcp-terraform-agent"
+  }
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "vm-e2-micro"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+  project      = var.project_id
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.custom_network.id
+    subnetwork = google_compute_subnetwork.custom_subnet.id
+    access_config {}
+  }
+
+  labels = {
+    creator = "gcp-terraform-agent"
+  }
 }
