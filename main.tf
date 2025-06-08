c0f193b06cfb6926bf0226f1076e8274c6f2b1d0
techwithhuz <138496939+techwithhuz@users.noreply.github.com>
2025-06-08 22:06:20 +0530

    Create main.tf with VM instance, VPC, subnet, firewall and enable necessary APIs

--- main.tf
+++ main.tf
@@ -1,16 +1,20 @@
 terraform {
+  required_version = ">= 1.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.0"
+      version = ">= 4.0"
     }
   }
-  required_version = ">= 1.1.0"
 }
 
 provider "google" {
   project = var.project_id
   region  = var.region
+  zone    = var.zone
+  labels = {
+    creator = "gcp-terraform-agent"
+  }
 }
 
 variable "project_id" {
@@ -24,29 +28,34 @@ variable "region" {
   default     = "us-central1"
 }
 
-resource "google_project_service" "compute_api" {
-  project = var.project_id
-  service = "compute.googleapis.com"
-  depends_on = []
-  disable_on_destroy = false
+variable "zone" {
+  description = "The GCP zone"
+  type        = string
+  default     = "us-central1-a"
 }
 
-resource "google_compute_network" "custom_network" {
-  name                    = "custom-vpc-network"
-  auto_create_subnetworks = false
-  project                 = var.project_id
+resource "google_project_service" "compute_api" {
+  service = "compute.googleapis.com"
+  project = var.project_id
+  disable_on_destroy = false
   labels = {
     creator = "gcp-terraform-agent"
   }
 }
 
-resource "google_compute_subnetwork" "custom_subnet" {
-  name          = "custom-subnet"
-  ip_cidr_range = "10.10.0.0/24"
-  region        = var.region
-  network       = google_compute_network.custom_network.id
-  project       = var.project_id
+resource "google_compute_network" "vpc_network" {
+  name = "custom-vpc-network"
+  auto_create_subnetworks = false
+  labels = {
+    creator = "gcp-terraform-agent"
+  }
+}
 
+resource "google_compute_subnetwork" "subnet" {
+  name          = "custom-subnet"
+  ip_cidr_range = "10.0.0.0/24"
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
   labels = {
     creator = "gcp-terraform-agent"
   }
@@ -54,26 +63,26 @@ resource "google_compute_subnetwork" "custom_subnet" {
 
 resource "google_compute_firewall" "allow_ssh" {
   name    = "allow-ssh"
-  network = google_compute_network.custom_network.id
-  project = var.project_id
+  network = google_compute_network.vpc_network.id
 
   allow {
     protocol = "tcp"
     ports    = ["22"]
   }
+
   source_ranges = ["0.0.0.0/0"]
 
-  direction = "INGRESS"
+  target_tags = ["allow-ssh"]
+  direction   = "INGRESS"
   labels = {
     creator = "gcp-terraform-agent"
   }
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "vm-e2-micro"
+resource "google_compute_instance" "default" {
+  name         = "tf-instance"
   machine_type = "e2-micro"
-  zone         = "us-central1-a"
-  project      = var.project_id
+  zone         = var.zone
 
   boot_disk {
     initialize_params {
@@ -82,12 +91,28 @@ resource "google_compute_instance" "vm_instance" {
   }
 
   network_interface {
-    network    = google_compute_network.custom_network.id
-    subnetwork = google_compute_subnetwork.custom_subnet.id
-    access_config {}
+    network    = google_compute_network.vpc_network.id
+    subnetwork = google_compute_subnetwork.subnet.id
+
+    access_config {
+      // Ephemeral public IP
+    }
   }
 
+  tags = ["allow-ssh"]
   labels = {
     creator = "gcp-terraform-agent"
   }
+
+  depends_on = [google_project_service.compute_api]
+}
+
+output "instance_name" {
+  value = google_compute_instance.default.name
+  description = "The name of the VM instance"
+}
+
+output "instance_external_ip" {
+  value = google_compute_instance.default.network_interface[0].access_config[0].nat_ip
+  description = "The external IP address of the VM"
 }
