8d89de1b81bf9ab5125f67554d81e4c7c8ab4582
techwithhuz <138496939+techwithhuz@users.noreply.github.com>
2025-06-09 10:49:55 +0530

    Add Terraform code for provisioning a GCP VM instance with firewall and network

--- main.tf
+++ main.tf
@@ -5,29 +5,77 @@ terraform {
       version = "~> 4.0"
     }
   }
-
   required_version = ">= 1.0"
 }
 
 provider "google" {
   project = var.project_id
-  region  = var.region
-  zone    = var.zone
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+resource "google_compute_network" "custom_network" {
+  name                    = "custom-vpc"
+  auto_create_subnetworks = false
+  labels = {
+    creator = "gcp-terraform-agent"
+  }
+}
+
+resource "google_compute_subnetwork" "custom_subnet" {
+  name          = "custom-subnet"
+  ip_cidr_range = "10.0.0.0/24"
+  network       = google_compute_network.custom_network.id
+  region        = "us-central1"
+  labels = {
+    creator = "gcp-terraform-agent"
+  }
+}
+
+resource "google_compute_firewall" "default_allow_ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.custom_network.name
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  direction     = "INGRESS"
+  labels = {
+    creator = "gcp-terraform-agent"
+  }
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "debian-vm"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.custom_network.id
+    subnetwork = google_compute_subnetwork.custom_subnet.id
+
+    access_config {}
+  }
+
+  labels = {
+    creator = "gcp-terraform-agent"
+  }
 }
 
 resource "google_project_service" "compute_api" {
-  project = var.project_id
   service = "compute.googleapis.com"
-
-  depends_on = [
-    google_project_service.compute_api
-  ]
-}
-
-variable "project_id" {}
-variable "region" {
-  default = "us-central1"
-}
-variable "zone" {
-  default = "us-central1-a"
+  disable_on_destroy = false
+  depends_on = [google_compute_network.custom_network]
 }
